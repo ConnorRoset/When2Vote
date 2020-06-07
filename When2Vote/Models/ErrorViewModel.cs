f42f76fb73bd9c6aa2de98d115a8a429cc89aeab
connorroset <croset.estem@gmail.com>
2020-06-07 18:37:14 -0500

    * .gitignore: Init
    * When2Vote.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * When2Vote.csproj:
    * site.js:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Privacy.cshtml:
    * Error.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * bootstrap-reboot.css.map:
    * bootstrap-reboot.min.css:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ When2Vote/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace When2Vote.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
